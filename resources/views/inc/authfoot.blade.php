c226ed2c31e21c946757ca793e9cdf1b2f3d6300
thakuratul2 <pratapsinghatul111@gmail.com>
2023-06-28 10:18:32 +0530

    Auth Setup done

--- resources/views/inc/authfoot.blade.php
+++ resources/views/inc/authfoot.blade.php
@@ -1,12 +1,12 @@
-<script src="../../vendors/js/vendor.bundle.base.js"></script>
+<script src="{{asset('assets/vendors/js/vendor.bundle.base.js')}}"></script>
 
-  <script src="../../vendors/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+  <script src="{{asset('assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js')}}"></script>
   
-  <script src="../../js/off-canvas.js"></script>
-  <script src="../../js/hoverable-collapse.js"></script>
-  <script src="../../js/template.js"></script>
-  <script src="../../js/settings.js"></script>
-  <script src="../../js/todolist.js"></script>
+  <script src="{{asset('assets/js/off-canvas.js')}}"></script>
+  <script src="{{asset('assets/js/hoverable-collapse.js')}}"></script>
+  <script src="{{asset('assets/js/template.js')}}"></script>
+  <script src="{{asset('assets/js/settings.js')}}"></script>
+  <script src="{{asset('assets/js/todolist.js')}}"></script>
  
 </body>
 
---
+++ resources/views/register.blade.php
@@ -0,0 +1,55 @@
+@include('inc.authnav')
+
+
+<div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+      <div class="content-wrapper d-flex align-items-center auth px-0">
+        <div class="row w-100 mx-0">
+          <div class="col-lg-4 mx-auto">
+            <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+              <div class="brand-logo">
+                <img src="{{asset('assets/img/logo.svg')}}" alt="logo">
+              </div>
+              <h4>Hello! let's get started</h4>
+              <h6 class="fw-light">Sign Up to continue.</h6>
+              <form class="pt-3" action="/register" method="POST">
+                <div class="form-group">
+                  <input type="email" class="form-control form-control-lg" id="email" name="email" placeholder="Email Address">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control form-control-lg" id="pass" name="password" placeholder="Password">
+                  </div>
+                <div class="form-group">
+                  <input type="password" class="form-control form-control-lg" id="cpass" name="confirm" placeholder="Confirm Password">
+                </div>
+                <div class="mt-3">
+                  <a class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" href="../../index.html">SIGN UP</a>
+                </div>
+                <div class="my-2 d-flex justify-content-between align-items-center">
+                  <div class="form-check">
+                    <label class="form-check-label text-muted">
+                      <input type="checkbox" class="form-check-input">
+                      Keep me signed in
+                    </label>
+                  </div>
+                 
+                </div>
+                <div class="mb-2">
+                  <button type="button" class="btn btn-block btn-facebook auth-form-btn">
+                    <i class="ti-facebook me-2"></i>Connect using facebook
+                  </button>
+                </div>
+                <div class="text-center mt-4 fw-light">
+                  Already have account? <a href="{{route('login.page')}}" class="text-primary">Login</a>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- content-wrapper ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+
+  @include('inc.authfoot')
